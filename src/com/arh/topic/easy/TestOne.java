7fd51cda1a4479f3604319987b0805dcc6a7b9f4
wudisorry <1192905560@qq.com>
2019-03-07 00:19:50 +0800

    3

--- src/com/arh/topic/easy/TestOne.java
+++ src/com/arh/topic/easy/TestOne.java
@@ -3,17 +3,52 @@ package com.arh.topic.easy;
 public class TestOne {
 
     public static void main(String[] args) {
-        reverse(2);
+        System.out.println(isPalindrome(1200021));
     }
 
     /**
-     *[7] 整数反转
+     * 判断一个整数是否是回文数。回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+     */
+    public static boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        } else if (x == 0) {
+            return true;
+        } else {
+            int number = 10;
+            int j = 1;
+            while (x / number > 0) {
+                int a = x / number;
+                int b = x % number;
+                int i = 0;
+                while (a / 10 >= Math.pow(10, j - 1)) {
+                    a = a / 10;
+                    i++;
+                }
+                if (number != 10) {
+                    a = a % (number / 10);
+                    b = b / (number / 10);
+                }
+
+                if (a != b) {
+                    return false;
+                }
+                j++;
+                number = (int) Math.pow(number, j);
+            }
+            return true;
+        }
+
+    }
+
+    /**
+     * [7] 整数反转
      * 给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。
      * 注意:
      * 假设我们的环境只能存储得下 32 位的有符号整数，则其数值范围为 [−2^31,  2^31 − 1]。请根据这个假设，如果反转后整数溢出那么就返回
      */
     public int reverse(int x) {
-
+        return 0;
     }
 
 
