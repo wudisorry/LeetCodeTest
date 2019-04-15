416c9f3a847a94e010c5daaadea47491241ab137
ARH <1192905560@qq.com>
2019-04-15 19:06:12 +0800

    whatever

---
+++ Note/SomeUrl.md
@@ -0,0 +1,2 @@
+#####1.window下使用Fiddler4抓包
+https://blog.csdn.net/xcqingfeng/article/details/78605399
--- src/com/arh/topic/easy/TestOne.java
+++ src/com/arh/topic/easy/TestOne.java
@@ -13,6 +13,43 @@ public class TestOne {
         System.out.println(to.isValid("]"));
     }
 
+    public int removeDuplicates(int[] nums) {
+        return 1;
+    }
+
+    /**
+     * Merge two sorted linked lists and return it as a new list. The new list
+     * should be made by splicing together the nodes of the first two lists.
+     * <p>
+     * Example:
+     * <p>
+     * Input: 1->2->4, 1->3->4
+     * Output: 1->1->2->3->4->4
+     */
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null)
+            return l2;
+        if (l2 == null)
+            return l1;
+        if (l1.val <= l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+
+    }
+
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
     /**
      * Given a string containing just the characters '(', ')', '{', '}', '[' and
      * ']', determine if the input string is valid.
